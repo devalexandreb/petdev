1bcb9133e5251ff318fe950cf67542a1ba8ecf59
Alexandre <alexandrebarbosacampos@gmail.com>
2025-04-20 01:34:59 -0300

    Update about.tsx

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -61,7 +61,7 @@ export function About() {
             <div className="flex gap-2">
               <a
                 target='_blank'
-                href={`https://wa.me/556799998800?text=Olá vim pelo site e gostaria de mais informações`}
+                href={`https://wa.me/5599999999999?text=Olá vim pelo site e gostaria de mais informações`}
                 className="bg-[#E84C3D] text-white flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md"
               >
                 <WhatsappLogo className="w-5 h-5 text-white" />
