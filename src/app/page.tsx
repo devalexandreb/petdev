92bcaae71667f8a7058a6e9e0a34377cc71833a6
Alexandre <alexandrebarbosacampos@gmail.com>
2025-04-19 23:04:48 -0300

    finalizando projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { About } from "./_components/about";
+import { Depoimentos } from "./_components/depoimento";
+import { Footer } from "./_components/footer";
 import { Hero } from "./_components/hero";
+import { Services } from "./_components/services";
 
 export default function Home(){
   return (
     <main>
       <Hero/>
       <About/>
+      <Services/>
+      <Depoimentos/>
+      <Footer/>
     </main>
   )
 }
